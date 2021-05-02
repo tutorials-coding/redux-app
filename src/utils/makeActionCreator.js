7dc31ae3d5694efc3327d0a21b1f3e6cf6c54005
Vadim <korobeinikovv@mail.ru>
2021-05-02 20:24:22 +0300

    Extend makeActionCreator for providing predefined values

--- src/utils/makeActionCreator.js
+++ src/utils/makeActionCreator.js
@@ -1,8 +1,11 @@
-export const makeActionCreator = (type, keys) => {
+export const makeActionCreator = (type, keys, valueProviders = {}) => {
   return (...values) => {
     const payload = {}
     keys.forEach((_, index) => {
       payload[keys[index]] = values[index]
+    })
+    Object.entries(valueProviders).forEach(([key, provideFn]) => {
+      payload[key] = provideFn()
     })
     return {
       type,
