dd1d38b920c6e8b2256dcf245faffaf6eff3131f
Vadim <korobeinikovv@mail.ru>
2021-05-02 18:51:28 +0300

    Implement new item add

--- src/store/todo-reducer.js
+++ src/store/todo-reducer.js
@@ -1,7 +1,12 @@
 import { todoList } from './mock-items'
+import { ADD_TODO } from './todo-actions'
 
 export const todoReducer = (state = todoList, action) => {
   switch (action.type) {
+    case ADD_TODO: {
+      const item = action.payload
+      return [...state, item]
+    }
     default:
       return state
   }
