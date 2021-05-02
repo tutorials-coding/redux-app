06ff7bcf305efddf7f7a23d38e73243f38e7b88b
Vadim <korobeinikovv@mail.ru>
2021-05-02 19:27:32 +0300

    Move items filter to useSelector

--- src/components/TodoList/TodoListContainer.jsx
+++ src/components/TodoList/TodoListContainer.jsx
@@ -4,10 +4,12 @@ import { useSelector } from 'react-redux'
 import { TodoList } from './TodoList'
 
 export function TodoListContainer() {
-  const items = useSelector((state) => state.todos)
-
-  const doneItems = items.filter((item) => item.done)
-  const todoItems = items.filter((item) => !item.done)
+  const doneItems = useSelector((state) =>
+    state.todos.filter((item) => item.done)
+  )
+  const todoItems = useSelector((state) =>
+    state.todos.filter((item) => !item.done)
+  )
 
   return <TodoList doneItems={doneItems} todoItems={todoItems} />
 }
