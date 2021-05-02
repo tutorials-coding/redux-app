49f0d4768d4971ca72294a9102599f4ebc87b07f
Vadim <korobeinikovv@mail.ru>
2021-05-02 20:23:46 +0300

    Apply makeActionCreator for update/delete

--- src/store/todo-actions.js
+++ src/store/todo-actions.js
@@ -1,4 +1,5 @@
 import { v4 as uuid } from 'uuid'
+import { makeActionCreator } from '../utils/makeActionCreator'
 
 export const ADD_TODO = 'ADD_TODO'
 export const UPDATE_TODO_TEXT = 'UPDATE_TODO_TEXT'
@@ -16,31 +17,14 @@ export const addTodo = (text) => {
   }
 }
 
-export const updateTodoText = (id, text) => {
-  return {
-    type: UPDATE_TODO_TEXT,
-    payload: {
-      id,
-      text,
-    },
-  }
-}
+export const updateTodoText = makeActionCreator(UPDATE_TODO_TEXT, [
+  'id',
+  'text',
+])
 
-export const updateTodoDone = (id, done) => {
-  return {
-    type: UPDATE_TODO_DONE,
-    payload: {
-      id,
-      done,
-    },
-  }
-}
+export const updateTodoDone = makeActionCreator(UPDATE_TODO_DONE, [
+  'id',
+  'done',
+])
 
-export const deleteTodo = (id) => {
-  return {
-    type: DELETE_TODO,
-    payload: {
-      id,
-    },
-  }
-}
+export const deleteTodo = makeActionCreator(DELETE_TODO, ['id'])
